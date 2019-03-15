45830aefb11ce566ff346f8b9cfecb5abf5eb7f3
ander.laverde.dev@gmail.com <ander.laverde.dev@gmail.com>
2019-03-15 11:17:24 -0500

    procedures

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Bases
 Archivos usados y notas de clase del curso Implementacion de Bases de Datos en el semestre 2019-1
+helouda
--- Scripts/crudAnderson.sql
+++ Scripts/crudAnderson.sql
@@ -3,43 +3,55 @@
 ***************** FACTURA ********************
 **********************************************/
 
-CREATE OR REPLACE FUNCTION VALIDAR_TARIFA(id_tarifa IN TARIFA.ID_TARIFA%TYPE) RETURN BOOLEAN
+CREATE OR REPLACE FUNCTION VALIDAR_TARIFA(
+                                          valor_minuto  tarifa.valor_minuto%type,
+                                          valor_mensaje  tarifa.valor_mensaje%type ,
+                                          valor_mb  tarifa.valor_mb%type,
+                                          valor_minuto_extra  tarifa.valor_minuto_extra%type,
+                                          valor_mensaje_extra  tarifa.valor_mensaje_extra%type,
+                                          valor_mb_extra  tarifa.valor_mb_extra%type) RETURN BOOLEAN
 IS
-    f_temporal VARCHAR(1);
+    f_temporal VARCHAR(5);
 BEGIN
     SELECT 'x'
     INTO f_temporal
     FROM tarifa
-    WHERE id_tarifa = nit;
+    WHERE tarifa.valor_minuto = valor_minuto 
+          and tarifa.valor_mensaje = valor_mensaje
+          and tarifa.valor_mb = valor_mb
+          and tarifa.valor_minuto_extra = valor_minuto_extra
+          and tarifa.valor_mensaje_extra = valor_mensaje_extra
+          and tarifa.valor_mb_extra = valor_mb_extra;
     RETURN TRUE;
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
         RETURN FALSE;
 END;
-
 /*******************CREAR***********************/
-CREATE OR REPLACE PROCEDURE REGISTRAR_TARIFA(  INTEGER,
-                                   nombre   char,
-                                   direccion varchar2,
-                                   telefono varchar2,
-                                   email varchar2,
-                                   codiciudad integer,
-                                   idcliempre integer,
-                                   idtipoclie integer)
+CREATE OR REPLACE PROCEDURE REGISTRAR_TARIFA( id_tarifa tarifa.id_tarifa%type,
+                                   valor_minuto  tarifa.valor_minuto%type,
+                                   valor_mensaje  tarifa.valor_mensaje%type ,
+                                   valor_mb  tarifa.valor_mb%type,
+                                   valor_minuto_extra  tarifa.valor_minuto_extra%type,
+                                   valor_mensaje_extra  tarifa.valor_mensaje_extra%type,
+                                   valor_mb_extra  tarifa.valor_mb_extra%type)
 IS
 BEGIN
-  IF VALIDAR_CLIENTE(nit)=FALSE THEN
+  IF VALIDAR_TARIFA(valor_minuto,valor_mensaje,valor_mb,valor_minuto_extra,valor_mensaje_extra,valor_mb_extra)=FALSE THEN
   BEGIN
-      INSERT INTO cliente (nit_clie,nom_clie,direccion_clie,tel_clie,email_clie,cod_ciudad,id_cliente_empresa,id_tipo_clie)
-      VALUES (nit,nombre,direccion,telefono,email,codiciudad,idcliempre,idtipoclie);
-      DBMS_OUTPUT.PUT_LINE('Insertado cliente: '|| nit || ' ' || nombre );
+      INSERT INTO tarifa (id_tarifa,valor_minuto,valor_mensaje,valor_mb,valor_minuto_extra,valor_mensaje_extra,valor_mb_extra)
+      VALUES (id_tarifa,valor_minuto,valor_mensaje,valor_mb,valor_minuto_extra,valor_mensaje_extra,valor_mb_extra);
+      DBMS_OUTPUT.PUT_LINE('Tarifa Insertada : '|| id_tarifa);
     END;
   ELSE
-    DBMS_OUTPUT.PUT_LINE(' cliente: '|| nit || ' ya existe en la base de datos');
+    DBMS_OUTPUT.PUT_LINE(' La Tarifa : '|| id_tarifa || ' ya existe en la base de datos');
   END IF;
   EXCEPTION
     WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE(' Se presento un error insertando cliente ');
-END REGISTRAR_CLIENTE;
+        DBMS_OUTPUT.PUT_LINE(' Se presento un error insertando la tarifa '||sqlerrm);
+END VALIDAR_TARIFA;
+
+/****************Eliminar*******************************/
+
 
 
